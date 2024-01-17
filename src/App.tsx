01dacce2a544f570676df28759638f33d8e08818
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2024-01-17 16:11:17 +0200

    Location input value exported

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import WeatherForecast from './Components/WeatherForecast';
 import LocationInput from './Components/LocationInput';
 
 function App() {
+  const handleLocationChange = (location: string) => {
+    console.log(`User Input: "${location}"`); // Print the location to the console
+  };
+
   return (
     <div className="App">
-      <WeatherToday/>
-      <WeatherForecast/>
-      <LocationInput/>
+      <WeatherToday />
+      <WeatherForecast />
+      <LocationInput onLocationChange={handleLocationChange} />
     </div>
   );
 }
--- src/Components/LocationInput.tsx
+++ src/Components/LocationInput.tsx
@@ -1,11 +1,12 @@
 import React, { useRef } from 'react';
 
-export default function LocationInput() {
+// Location input function exports onLocationChange value
+export default function LocationInput({ onLocationChange }: { onLocationChange: (location: string) => void }) {
     const locationInput = useRef<HTMLInputElement>(null); // Hook introduced with a null value
     // Prints the current value of locationInput when the button is pressed
     const searchWeather = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault(); // Prevents page refresh
-        console.log(`User Input: "${locationInput.current!.value}"`);
+        onLocationChange(locationInput.current!.value);
     };
 
     return (
