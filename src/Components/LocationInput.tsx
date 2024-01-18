9030042e0ec146426887325c637fd49bfb51881f
Kristopher Pepper <Bgp534@myy.haaga-helia.fi>
2024-01-18 11:21:29 +0200

    Forecast API fetch added

--- src/Components/LocationInput.tsx
+++ src/Components/LocationInput.tsx
@@ -1,17 +1,16 @@
 import React, { useRef, useEffect } from 'react';
 
 interface LocationInputProps {
-    onWeatherData: (weatherData: any) => void; // Callback for passing weather data
+    onWeatherData: (weatherData: boolean) => void; // Callback for passing weather data
 }
 
 export default function LocationInput({ onWeatherData }: LocationInputProps) {
     const locationInput = useRef<HTMLInputElement>(null);
+    let hasDefaultWeather = false;
 
-    // Passes the current value of locationInput to onLocationChange when the button is pressed
-    const searchWeather = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const weatherTodayFetch = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault(); // Prevents page refresh
 
-        // Fetches JSON from the given URL
         fetch('https://api.openweathermap.org/data/2.5/weather?q=' + locationInput.current!.value + '%20&APPID=d3b94e0efe686baf1916cbf041859138')
             .then(response => response.json())
             .then(responseData => {
@@ -20,8 +19,22 @@ export default function LocationInput({ onWeatherData }: LocationInputProps) {
                     return;
                 }
                 onWeatherData(responseData);
+                const lat = responseData.coord.lat;
+                const lon = responseData.coord.lon;
+                weatherForecastFetch(lat, lon);
             })
-            .catch(err => {// Something went wrong
+            .catch(err => {
+                console.log(err);
+            });
+    };
+
+    const weatherForecastFetch = (lat: number, lon: number) => {
+        fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + 'd3b94e0efe686baf1916cbf041859138')
+            .then(response => response.json())
+            .then(responseData => {
+                console.log(responseData);
+            })
+            .catch(err => {
                 console.log(err);
             });
     };
@@ -29,11 +42,13 @@ export default function LocationInput({ onWeatherData }: LocationInputProps) {
     useEffect(() => {
         // Set the default value for the input field
         locationInput.current!.value = "Helsinki";
-        
-        // Manually trigger the search when the component mounts
-        searchWeather(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }) as any);
+
+        if (hasDefaultWeather === false) {
+            weatherTodayFetch(new MouseEvent('click', { bubbles: true, cancelable: true, view: window }) as any);
+            hasDefaultWeather = true;
+        }
     }, []);
-    
+
     return (
         <>
             <div>
@@ -43,7 +58,7 @@ export default function LocationInput({ onWeatherData }: LocationInputProps) {
                         placeholder="Location"
                         ref={locationInput} // Value sets the "locationInput" variable
                     />
-                    <button onClick={searchWeather}>
+                    <button onClick={weatherTodayFetch}>
                         Search
                     </button>
                 </form>
